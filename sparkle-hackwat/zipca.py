ec5660ac85dee72cb1218786cb81437aac49fcfb
Andrew Sullivan Cant <acant@alumni.uwaterloo.ca>
2011-04-09 13:08:49 -0400

    Finished the initial zip.ca search functionality.

--- sparkle-hackwat/zipca.py
+++ sparkle-hackwat/zipca.py
@@ -1,2 +1,17 @@
+import urllib
+import logging
+import json
+from google.appengine.api import urlfetch
+
 def search(title, upc):
-  return '000000000'
+  search = upc
+  if search_term == 'None'
+    search_term = title
+
+  params = urllib.urlencode({'page': 1, 'pagesize': 1, 'searchTerm': search_term})
+  result = urlfetch.fetch("http://hackott.zip.ca/opencatalog/search?%s" % params)
+
+  if result.status_code == 200:
+    return json.loads(result.content)['CatalogTitles'][0]
+
+  return {}
