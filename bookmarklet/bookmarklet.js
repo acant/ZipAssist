9a8a17cae042b6dceefb492fc7b77c30ccafba6b
Michal Mocny <mmocny@gmail.com>
2011-04-09 11:25:35 -0400

    minor tweak

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -6,7 +6,7 @@
 		var title = encodeURIComponent(document.title);
 		var selected = encodeURIComponent(window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text );
 		var w = window.open(
-			'/opt/src/ZipAssist/bookmarklet/success.html?url='+url+'&title='+title+'&selected='+selected,
+			'file:///opt/src/ZipAssist/bookmarklet/success.html?url='+url+'&title='+title+'&selected='+selected,
 			'ZipAssist',
 			'toolbar=no,scrollbar=yes,height='+height+',width='+width+',top='+((screen.height-height)/2)+',left='+((screen.width-width)/2));
 	}
