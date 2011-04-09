3b2a1bb073ca249d8f05cbb49a514196d131fba7
Andrew Sullivan Cant <acant@alumni.uwaterloo.ca>
2011-04-09 13:48:37 -0400

    Fixed the search functionality.

--- sparkle-hackwat/zipca.py
+++ sparkle-hackwat/zipca.py
@@ -4,14 +4,14 @@ import json
 from google.appengine.api import urlfetch
 
 def search(title, upc):
-  search = upc
-  if search_term == 'None'
-    search_term = title
-
-  params = urllib.urlencode({'page': 1, 'pagesize': 1, 'searchTerm': search_term})
+  params = urllib.urlencode({'page': 1, 'pagesize': 1, 'searchTerm': title.lower()})
   result = urlfetch.fetch("http://hackott.zip.ca/opencatalog/search?%s" % params)
 
   if result.status_code == 200:
-    return json.loads(result.content)['CatalogTitles'][0]
+    titles = json.loads(result.content)['CatalogTitles']
+    if len(titles) != 0:
+      return titles[0]
+    else:
+      return {}
 
   return {}
