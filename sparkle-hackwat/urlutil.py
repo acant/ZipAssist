4ebdd85cd88170b2a8d45954ff5501fda71cd45b
Michal Mocny <mmocny@gmail.com>
2011-04-09 12:42:31 -0400

    better get_movie_title implementation for imdb

--- sparkle-hackwat/urlutil.py
+++ sparkle-hackwat/urlutil.py
@@ -1,10 +1,42 @@
+import urllib2
 
+################################################################################
+# Python 'enum'
+class SpecialPages():
+	NONE = 0
+	IMDB_MOVIE_PAGE = 1
+
+def map_url_to_special_page(url):
+	return SpecialPages.IMDB_MOVIE_PAGE
+
+################################################################################
+# Utils
+
+def imdb_movie_page_title_to_movie_title(title):
+	return title[:title.rfind('(')-1]
+
+################################################################################
+# The bread and butter function right here:
 
 def get_movie_title_and_upc_from_url(title,url,selected):
+	title = urllib2.unquote(title)
+	url = urllib2.unquote(url)
+	selected = urllib2.unquote(selected)
+
+	# This overrides everything else
 	if selected:
 		return selected, None
-	
+
+	sp = map_url_to_special_page(url)
+
+	if sp is SpecialPages.NONE:
+		return None, None
+	elif sp is SpecialPages.IMDB_MOVIE_PAGE:
+		return imdb_movie_page_title_to_movie_title(title), None
+
 	return None, None
+
+################################################################################
 
 def test_with_sample_imdb_page():
 	tests = [
