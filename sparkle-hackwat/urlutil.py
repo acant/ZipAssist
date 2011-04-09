18ee86a411651ba8e5daecb519aec54a0f5b4db5
Michal Mocny <mmocny@gmail.com>
2011-04-09 12:32:14 -0400

    added some tests to get_movie_title_and_upc_from_url

--- sparkle-hackwat/urlutil.py
+++ sparkle-hackwat/urlutil.py
@@ -1,8 +1,27 @@
 
 
 def get_movie_title_and_upc_from_url(title,url,selected):
-	return selected, None
-
+	if selected:
+		return selected, None
+	
+	return None, None
 
 def test_with_sample_imdb_page():
-	pass
+	tests = [
+		{
+			'url': 'http%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt1411704%2F',
+			'title': 'Hop%20(2011)%20-%20IMDb',
+			'selected': '',
+		},
+		{
+			'url': 'http%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt1411704%2F',
+			'title': 'Hop%20(2011)%20-%20IMDb',
+			'selected': 'Hop',
+		},
+	]
+
+	for url,selected,title in (test.values() for test in tests):
+		print get_movie_title_and_upc_from_url(title,url,selected)
+
+if __name__ == "__main__":
+	test_with_sample_imdb_page()
